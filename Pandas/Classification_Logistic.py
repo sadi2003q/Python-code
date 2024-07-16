c5c84f23a315bab8154ca5ca6271ff25721a6dff
Md. Adnan Abdullah <Adnanabdullah625@gmail.com>
2024-07-16 10:58:41 +0600

    DOne

--- Pandas/Classification_Logistic.py
+++ Pandas/Classification_Logistic.py
@@ -1,37 +1,31 @@
+# %%
 import pandas as pd
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, \
     classification_report
-from sklearn.model_selection import cross_val_predict, cross_val_score, KFold
+from sklearn.model_selection import cross_val_score
 
 
+# %%
 class ClassificationMetrics:
-    def __init__(self, model, X, y, cv=100):
-        self.model = model
-        self.X = X
-        self.y = y
+    def __init__(self, model, Y_test, Y_prediction, cv=10):
         self.cv = cv
-        self.kf = KFold(n_splits=cv, shuffle=True, random_state=42)
+        self.model = model
+        self.Y_test = Y_test
+        self.Y_prediction = Y_prediction
 
-    def get_cross_val_metrics(self):
-        accuracy = cross_val_score(self.model, self.X, self.y, cv=self.cv, scoring='accuracy')
-        precision = cross_val_score(self.model, self.X, self.y, cv=self.cv, scoring='precision_weighted')
-        recall = cross_val_score(self.model, self.X, self.y, cv=self.cv, scoring='recall_weighted')
-        f1 = cross_val_score(self.model, self.X, self.y, cv=self.cv, scoring='f1_weighted')
+    def get_score(self):
 
         metrics = {
-            'Fold': range(1, self.cv + 1),
-            'Accuracy': accuracy,
-            'Precision': precision,
-            'Recall': recall,
-            'F1 Score': f1
+            'Accuracy': accuracy_score(self.Y_test, self.Y_prediction),
+            'Precision': precision_score(self.Y_test, self.Y_prediction, average='weighted'),
+            'Recall': recall_score(self.Y_test, self.Y_prediction, average='weighted'),
+            'F1 Score': f1_score(self.Y_test, self.Y_prediction, average='weighted')
         }
-        return pd.DataFrame(metrics)
+        return pd.DataFrame([metrics])
 
     def get_confusion_matrix(self):
-        y_pred = cross_val_predict(self.model, self.X, self.y, cv=self.cv)
-        return pd.DataFrame(confusion_matrix(self.y, y_pred))
+        return pd.DataFrame(confusion_matrix(self.Y_test, self.Y_prediction))
 
     def get_classification_report(self):
-        y_pred = cross_val_predict(self.model, self.X, self.y, cv=self.cv)
-        report_dict = classification_report(self.y, y_pred, output_dict=True)
+        report_dict = classification_report(self.Y_test, self.Y_prediction, output_dict=True)
         return pd.DataFrame(report_dict).transpose()
--- Pandas/Classification_Regressor.py
+++ Pandas/Classification_Regressor.py
@@ -1,34 +1,25 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, make_scorer
-from sklearn.model_selection import cross_val_score, cross_val_predict, KFold
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import cross_val_score
 
 
 class Regression:
-    def __init__(self, model, X, y, cv=100):
-        self.model = model
-        self.X = X
-        self.y = y
+    def __init__(self, model, test_y, prediction, cv=100):
         self.cv = cv
-        self.kf = KFold(n_splits=cv, shuffle=True, random_state=42)
+        self.model = model
+        self.test_y = test_y
+        self.prediction = prediction
 
-    def get_cross_val_metrics(self):
-        r2_scorer = make_scorer(r2_score)
-        mae_scorer = make_scorer(mean_absolute_error)
-        mse_scorer = make_scorer(mean_squared_error)
-
-        r2_scores = cross_val_score(self.model, self.X, self.y, cv=self.cv, scoring=r2_scorer)
-        mae_scores = cross_val_score(self.model, self.X, self.y, cv=self.cv, scoring=mae_scorer)
-        mse_scores = cross_val_score(self.model, self.X, self.y, cv=self.cv, scoring=mse_scorer)
+    def get_score(self):
+        r2 = r2_score(self.test_y, self.prediction)
+        mean_square = mean_squared_error(self.test_y, self.prediction)
+        mean_absolute = mean_absolute_error(self.test_y, self.prediction)
 
         metrics = {
-            'Fold': range(1, self.cv + 1),
-            'R2 Score': r2_scores,
-            'Mean Absolute Error': mae_scores,
-            'Mean Squared Error': mse_scores
+            "r2_score": r2,
+            "mean_absolute_error": mean_square,
+            "mean_squared_error": mean_absolute,
         }
-        return pd.DataFrame(metrics)
 
-    def get_cross_val_predictions(self):
-        y_pred = cross_val_predict(self.model, self.X, self.y, cv=self.cv)
-        return pd.DataFrame({'Actual': self.y, 'Predicted': y_pred})
+        return pd.DataFrame([metrics])  # Return as a DataFrame with one row
